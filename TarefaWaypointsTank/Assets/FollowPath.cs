4ec0e169e240e6225381c9438221a8a9491a600f
Henrique <henriqueanselmo1011@gmail.com>
2021-04-16 14:45:12 -0300

    Tarefa de waypoints com NavMesh

--- TarefaWaypointsTank/Assets/FollowPath.cs
+++ TarefaWaypointsTank/Assets/FollowPath.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class FollowPath : MonoBehaviour
 {
-    //Var que seja o Objetivo
+
+    /*/Var que seja o Objetivo
     Transform goal;
 
     //Não utilizado pra nada
@@ -15,6 +16,7 @@ public class FollowPath : MonoBehaviour
 
     //Velocidade da rotação
     float rotSpeed = 2.0f;
+    /*/
 
     //Var para guardar o GameObject com o script WPManager
     public GameObject wpManager;
@@ -22,82 +24,102 @@ public class FollowPath : MonoBehaviour
     //Array com os waypoints
     GameObject[] wps;
 
-    //Marca o Nó atual
+    //Instancia o NavmeshAgent
+    UnityEngine.AI.NavMeshAgent agent;
+
+    /*/Marca o Nó atual
     GameObject currentNode;
+
 
     //Setando o Wp atual pra 0
     int currentWP = 0;
 
     //Variavel para o Graph
     Graph g;
+    /*/
 
     void Start()
     {
-        //Pegando as variaveis existentes no script WPManager
+        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
         wps = wpManager.GetComponent<WPManager>().waypoints;
+
+        /*/ Pegando as variaveis existentes no script WPManager
         g = wpManager.GetComponent<WPManager>().graph;
 
         //Setando o Nó pra wps 0
         currentNode = wps[0];
+        /*/
     }
 
     // Método linkado ao button para fazer o tank ir ate o Heliporto
     public void GoToHeli()
     {
-        //Faz o Caminho do nó atual até o locar objetivo
+        agent.SetDestination(wps[9].transform.position);
+
+        /*/
+        Faz o Caminho do nó atual até o locar objetivo
         g.AStar(currentNode, wps[9]);
         //Setando o Wp atual pra 0
         currentWP = 0;
+        /*/
     }
 
     // Método linkado ao button para fazer o tank ir ate as ruinas 
     public void GoToRuin()
     {
-        //Faz o Caminho do nó atual até o locar objetivo
+        agent.SetDestination(wps[2].transform.position);
+        /*/
+        Faz o Caminho do nó atual até o locar objetivo
         g.AStar(currentNode, wps[2]);
         //Setando o Wp atual pra 0
         currentWP = 0;
+        /*/
     }
 
     // Método linkado ao button para fazer o tank ir ate a fabrica 
     public void GoToFab()
     {
-        //Faz o Caminho do nó atual até o locar objetivo
+        agent.SetDestination(wps[6].transform.position);
+        /*/
+        Faz o Caminho do nó atual até o locar objetivo
         g.AStar(currentNode, wps[6]);
         //Setando o Wp atual pra 0
         currentWP = 0;
+        /*/
     }
 
-    void LateUpdate()
-    {
-        //Utilizando o graph
-        if (g.getPathLength() == 0 || currentWP == g.getPathLength())
-          return;
+    /*/
+         void LateUpdate()
+     {
+         //Utilizando o graph
+         if (g.getPathLength() == 0 || currentWP == g.getPathLength())
+           return;
 
-        //Ve o nó mais proximo
-        currentNode = g.getPathPoint(currentWP);
+         //Ve o nó mais proximo
+         currentNode = g.getPathPoint(currentWP);
 
-        //Calcula a distancia de dois vetores e se menor que a accuracy, soma 1 ao current waypoint
-        if (Vector3.Distance(g.getPathPoint(currentWP).transform.position,transform.position) < accuracy)
-        {
-            currentWP++;
-        }
+         //Calcula a distancia de dois vetores e se menor que a accuracy, soma 1 ao current waypoint
+         if (Vector3.Distance(g.getPathPoint(currentWP).transform.position,transform.position) < accuracy)
+         {
+             currentWP++;
+         }
 
-        //verifica se o Wp atual é menor que o g.getPathLength
-        if (currentWP < g.getPathLength())
-        {
-            //define o objetivo
-            goal = g.getPathPoint(currentWP).transform;
+         //verifica se o Wp atual é menor que o g.getPathLength
+         if (currentWP < g.getPathLength())
+         {
+             //define o objetivo
+             goal = g.getPathPoint(currentWP).transform;
 
-            //Vira ele pro objetivo
-            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y,goal.position.z);
+             //Vira ele pro objetivo
+             Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y,goal.position.z);
 
-            //Se dirige ate o objetivo
-            Vector3 direction = lookAtGoal - this.transform.position; this.transform.rotation = Quaternion.Slerp(this.transform.rotation,Quaternion.LookRotation(direction),Time.deltaTime * rotSpeed);
-        }
+             //Se dirige ate o objetivo
+             Vector3 direction = lookAtGoal - this.transform.position; this.transform.rotation = Quaternion.Slerp(this.transform.rotation,Quaternion.LookRotation(direction),Time.deltaTime * rotSpeed);
+         }
 
-        //Faz o player se mover / Professor não colocou no slide
+         //Faz o player se mover / Professor não colocou no slide
 
-        this.transform.Translate(0, 0, speed * Time.deltaTime);
-    }
+         this.transform.Translate(0, 0, speed * Time.deltaTime);
+     }
+     /*/
 }
